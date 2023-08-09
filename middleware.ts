ebd93e2e6152316a06fb980d4707a57d27a5d90e
yinkakun <yinkakun@gmail.com>
2023-08-09 17:53:50 +0300

    chore(middleware): temporarily disable protected route interception

---
+++ middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+import { decodeJWTPayload } from '@/lib/utils';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('jwt');
+
+  if (!token) return redirectToLogin(request);
+
+  const payload = decodeJWTPayload(token.value);
+  if (payload.exp < Date.now() / 1000) return redirectToLogin(request);
+
+  return NextResponse.next();
+}
+
+const redirectToLogin = (request: NextRequest) => {
+  const redirectUrl = `/login?from=${request.nextUrl.pathname}`;
+  return NextResponse.redirect(new URL(redirectUrl, request.url));
+};
+
+export const config = {
+  matcher: [
+    '/xoverview/:path*',
+    '/xjobs/:path*',
+    '/xwallet/:path*',
+    '/xtalents/:path*',
+    '/xsettings/:path*',
+    '/xmessages/:path*',
+    '/xprojects/:path*',
+  ],
+};
