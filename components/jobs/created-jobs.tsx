8421832efa5cf5284f60b07e0e7f9b7135d448bb
yinkakun <yinkakun@gmail.com>
2023-08-15 19:11:47 +0300

    feat(jobs): update jobs components

--- components/jobs/created-jobs.tsx
+++ components/jobs/created-jobs.tsx
@@ -1,54 +1,71 @@
 'use client';
 
 import React from 'react';
-import Link from 'next/link';
-import { Button, Input } from 'pakt-ui';
-import { Plus } from 'lucide-react';
-import { PublicJob } from '@/components/jobs';
-import { Tabs } from '@/components/common/tabs';
 import { Job } from '@/components/jobs/';
-import { JobSearchBar } from '@/components/jobs/job-search-bar';
+import { Tabs } from '@/components/common/tabs';
 
 interface Props {}
 
 export const CreatedJobs: React.FC<Props> = () => {
   return (
     <div className="flex flex-col gap-6">
-      <JobSearchBar />
       <div>
         <Tabs
+          urlKey="my-jobs"
           tabs={[
             {
-              label: 'All',
-              value: 'all',
-              content: (
-                <div className="grid grid-cols-2 gap-4">
-                  <Job
-                    type="assigned"
-                    inviter={{
-                      avatar: 'https://i.pravatar.cc/300',
-                      name: 'John Doe',
-                      paktScore: 100,
-                    }}
-                    price={100}
-                    title='I need a logo for my new business "Pakt"'
-                  />
-                  <Job
-                    type="unassigned"
-                    createdAt="2 days ago"
-                    price={100}
-                    title='I need a logo for my new business "Pakt"'
-                  />
-                </div>
-              ),
+              label: 'Ongoing',
+              value: 'ongoing',
+              content: <OngoingJobs />,
             },
-
-            { label: 'Ongoing', value: 'ongoing', content: <div>Ongoing</div> },
-            { label: 'Completed', value: 'completed', content: <div>Completed</div> },
-            { label: 'Unassigned', value: 'unassigned', content: <div>Unassigned</div> },
+            { label: 'Completed', value: 'completed', content: <CompletedJobs /> },
+            { label: 'Unassigned', value: 'unassigned', content: <UnassignedJobs /> },
           ]}
         />
       </div>
+    </div>
+  );
+};
+
+const UnassignedJobs = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <Job type="unassigned" createdAt="2 days ago" price={100} title='I need a logo for my new business "Pakt"' />
+      <Job type="unassigned" createdAt="2 days ago" price={100} title='I need a logo for my new business "Pakt"' />
+    </div>
+  );
+};
+
+const OngoingJobs = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <Job
+        type="assigned"
+        inviter={{
+          avatar: 'https://i.pravatar.cc/300',
+          name: 'John Doe',
+          paktScore: 100,
+        }}
+        price={100}
+        title='I need a logo for my new business "Pakt"'
+      />
+    </div>
+  );
+};
+
+const CompletedJobs = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <Job
+        type="assigned"
+        inviter={{
+          avatar: 'https://i.pravatar.cc/300',
+          name: 'John Doe',
+          paktScore: 100,
+        }}
+        price={100}
+        title='I need a logo for my new business "Pakt"'
+      />
     </div>
   );
 };
--- components/jobs/open-jobs.tsx
+++ components/jobs/open-jobs.tsx
@@ -1,7 +1,89 @@
 import React from 'react';
+import { Tabs } from '@/components/common/tabs';
+import { PublicJobCard } from '@/components/jobs';
+import { JobSearchBar } from '@/components/jobs/job-search-bar';
 
 interface Props {}
 
 export const OpenJobs: React.FC<Props> = () => {
-  return <div>Open Jobs</div>;
+  return (
+    <div className="flex flex-col gap-6">
+      <JobSearchBar />
+      <div>
+        <Tabs
+          urlKey="open-jobs"
+          tabs={[
+            {
+              label: 'All',
+              value: 'all',
+              content: <AllJobs />,
+            },
+            {
+              label: 'Saved',
+              value: 'saved',
+              content: <SavedJobs />,
+            },
+          ]}
+        />
+      </div>
+    </div>
+  );
+};
+
+interface AllJobsProps {}
+
+const AllJobs: React.FC<AllJobsProps> = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <PublicJobCard
+        price={100}
+        title='I need a logo for my new business "Pakt"'
+        skills={['Logo Design', 'Graphic Design', 'Illustration']}
+        creator={{
+          avatar: 'https://i.pravatar.cc/300',
+          name: 'John Doe',
+          paktScore: 100,
+        }}
+      />
+      <PublicJobCard
+        price={100}
+        title='I need a logo for my new business "Pakt"'
+        skills={['Logo Design', 'Graphic Design', 'Illustration']}
+        creator={{
+          avatar: 'https://i.pravatar.cc/300',
+          name: 'John Doe',
+          paktScore: 100,
+        }}
+      />
+      <PublicJobCard
+        price={100}
+        title='I need a logo for my new business "Pakt"'
+        skills={['Logo Design', 'Graphic Design', 'Illustration']}
+        creator={{
+          avatar: 'https://i.pravatar.cc/300',
+          name: 'John Doe',
+          paktScore: 100,
+        }}
+      />
+    </div>
+  );
+};
+
+interface SavedJobsProps {}
+
+const SavedJobs: React.FC<SavedJobsProps> = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      <PublicJobCard
+        price={100}
+        title='I need a logo for my new business "Pakt"'
+        skills={['Logo Design', 'Graphic Design', 'Illustration']}
+        creator={{
+          avatar: 'https://i.pravatar.cc/300',
+          name: 'John Doe',
+          paktScore: 100,
+        }}
+      />
+    </div>
+  );
 };
