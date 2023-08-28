12e00dee24b235d469efa0d86e0529eaabcd4519
yinkakun <yinkakun@gmail.com>
2023-08-28 21:19:08 +0300

    refactor(account): move User interface to separate file

--- lib/store/account.ts
+++ lib/store/account.ts
@@ -1,66 +1,27 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { create } from 'zustand';
+import { User } from '@/lib/types';
 
-export interface IUser {
-  _id: string;
-  type: string;
-  email: string;
-  lastName: string;
-  firstName: string;
-  score: number;
-  profileCompleteness: number;
-  profileImage?: {
-    url: string;
-  };
-  profile: {
-    contact?: {
-      city?: string;
-      state?: string;
-      phone?: string;
-      address?: string;
-      country?: string;
-    };
-    bio?: {
-      title?: string;
-      description?: string;
-    };
-    talent: {
-      availability: string;
-      skills: string[];
-      skillIds: any[];
-      about?: string;
-    };
-  };
-}
-
-type UserState = IUser & {
-  setUser: (user: IUser) => void;
+type UserState = User & {
+  setUser: (user: User) => void;
 };
 
-export const useUserState = create<UserState>()(
-  persist(
-    (set) => ({
-      _id: "",
-      type: "",
-      email: "",
-      lastName: "",
-      firstName: "",
-      score: 0,
-      profileImage: {
-        url: "",
-      },
-      profileCompleteness: 0,
-      profile: {
-        talent: {
-          skillIds: [],
-          availability: "",
-          skills: [],
-        },
-      },
-      setUser: (user: IUser) => set(user),
-    }),
-    {
-      name: "user",
-    }
-  )
-);
+export const useUserState = create<UserState>()((set) => ({
+  _id: '',
+  type: '',
+  email: '',
+  lastName: '',
+  firstName: '',
+  score: 0,
+  profileImage: {
+    url: '',
+  },
+  profileCompleteness: 0,
+  profile: {
+    talent: {
+      skillIds: [],
+      availability: '',
+      skills: [],
+    },
+  },
+  setUser: (user: User) => set(user),
+}));
