615f2ae6e97ef23801ffc6bfe67383bb1d23b330
yinkakun <yinkakun@gmail.com>
2023-09-06 18:33:16 +0300

    feat(job-creation): refine job creation process

--- lib/store/job-creation.ts
+++ lib/store/job-creation.ts
@@ -2,58 +2,70 @@ import { create } from 'zustand';
 
 export type JobCreationSteps = 'details' | 'deliverables' | 'project' | 'visibility' | 'review';
 
+const STEPS: JobCreationSteps[] = ['details', 'deliverables', 'project', 'visibility', 'review'];
+
 type State = {
   job: {
-    due?: string;
-    title?: string;
-    budget?: number;
-    category?: string;
-    skills?: string[];
-    description?: string;
-    deliverables?: string[];
-    type?: 'freelance' | 'project';
-    visibility?: 'public' | 'private';
+    title: string;
+    budget: string;
+    category: string;
+    skills: string[];
+    description: string;
+    due: Date | undefined;
+    deliverables: string[];
+    type: 'freelance' | 'project';
+    visibility: 'public' | 'private';
   };
+  activeStepIndex: number;
   activeStep: JobCreationSteps;
-  stepsStatus: Record<JobCreationSteps, 'pending' | 'active' | 'complete'>;
+  completedSteps: JobCreationSteps[];
+  isActiveStep: (step: JobCreationSteps) => boolean;
+  isCompletedStep: (step: JobCreationSteps) => boolean;
 };
 
 type Actions = {
-  resetJob: () => void;
+  resetJobCreation: () => void;
   setJob: (job: State['job']) => void;
-  setActiveStep: (step: State['activeStep']) => void;
-  setStepsStatus: (payload: Partial<State['stepsStatus']>) => void;
+  gotoNextStep: () => void;
+  gotoStep: (step: JobCreationSteps) => void;
 };
 
 export const useJobCreationStore = create<State & Actions>((set, get) => ({
   job: {
     title: '',
-    dueDate: '',
+    budget: '',
+    skills: [],
     category: '',
-    skillSets: [],
-    proposedPrice: 0,
-    deliverables: [],
+    due: undefined,
     description: '',
-    jobType: 'freelance',
+    deliverables: [],
+    type: 'freelance',
     visibility: 'public',
   },
+  completedSteps: [],
+  activeStepIndex: 0,
   activeStep: 'details',
-  stepsStatus: {
-    details: 'active',
-    review: 'pending',
-    project: 'pending',
-    visibility: 'pending',
-    deliverables: 'pending',
-  },
-  setStepsStatus: (payload) => {
-    set((state) => ({
-      stepsStatus: {
-        ...state.stepsStatus,
-        ...payload,
-      },
-    }));
-  },
-  resetJob: () => set({}),
-  setActiveStep: (step) => set({ activeStep: step }),
+  resetJobCreation: () => set({}),
   setJob: (job) => set({ job: { ...get().job, ...job } }),
+  gotoNextStep: () => {
+    // goes to the next step, sets the current step as completed and next step as active
+    const { activeStepIndex, completedSteps } = get();
+
+    if (activeStepIndex < STEPS.length - 1) {
+      set({
+        activeStepIndex: activeStepIndex + 1,
+        activeStep: STEPS[activeStepIndex + 1],
+        completedSteps: [...completedSteps, STEPS[activeStepIndex]],
+      });
+    }
+  },
+  gotoStep: (step) => {
+    // goes to the step if it's completed or active
+    const { completedSteps, activeStep } = get();
+    if (completedSteps.includes(step) || activeStep === step) {
+      set({ activeStep: step, activeStepIndex: STEPS.indexOf(step) });
+    }
+  },
+  isActiveStep: (step) => get().activeStep === step,
+  isCompletedStep: (step) => get().completedSteps.includes(step),
 }));
