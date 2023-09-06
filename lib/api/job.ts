b6cfd5451c33663bc613a6f3a4bb9ffe31ad81de
yinkakun <yinkakun@gmail.com>
2023-09-06 18:28:31 +0300

    feat(job): update job creation and update process

--- lib/api/job.ts
+++ lib/api/job.ts
@@ -1,129 +1,76 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import type { Job } from '@/lib/types';
 import { axios, ApiError, ApiResponse } from '@/lib/axios';
 import { toast } from '@/components/common/toaster';
-// Create Job
+import { useMutation, useQuery } from '@tanstack/react-query';
 
+// Create Job
 interface CreateJobParams {
-  type: 'job';
   name: string;
   category: string;
   paymentFee: number;
   description: string;
   isPrivate: boolean;
   deliveryDate: string;
-  attachments: string[];
-  skills: string[];
+  tags?: string[];
+  attachments?: string[];
+  deliverables: string[];
 }
 
-interface CreateJobResponse {
-  id: string;
-}
-
-async function postCreateJob(params: CreateJobParams): Promise<CreateJobResponse> {
-  const res = await axios.post('/collection', params);
+async function postCreateJob(params: CreateJobParams): Promise<Job> {
+  const res = await axios.post('/collection', {
+    ...params,
+    type: 'job',
+    deliverables: undefined,
+  });
   return res.data.data;
 }
 
 export function useCreateJob() {
+  const assignJobDeliverables = useAssignJobDeliverables();
+
   return useMutation({
     mutationFn: postCreateJob,
     mutationKey: ['create-job'],
     onError: (error: ApiError) => {
       toast.error(error?.response?.data.message || 'An error occurred');
     },
-    onSuccess: () => {
-      toast.success('Job created successfully');
-    },
-  });
-}
-
-// Update Job
-
-interface UpdateJobParams {
-  jobId: string;
-  type: 'job';
-  name?: string;
-  category?: string;
-  paymentFee?: number;
-  description?: string;
-  isPrivate?: boolean;
-  deliveryDate?: string;
-  attachments?: string[];
-  skills?: string[];
-}
-
-interface UpdateJobResponse {
-  id: string;
-}
-
-async function postUpdateJob(params: UpdateJobParams): Promise<UpdateJobResponse> {
-  const res = await axios.patch(`/collection/${params.jobId}`, params);
-  return res.data.data;
-}
-
-export function useUpdateJob() {
-  return useMutation({
-    mutationFn: postUpdateJob,
-    mutationKey: ['update-job'],
-    onError: (error: ApiError) => {
-      toast.error(error?.response?.data.message || 'An error occurred');
-    },
-    onSuccess: () => {
-      toast.success('Job updated successfully');
+    onSuccess: ({ _id, name }, { deliverables = [] }) => {
+      assignJobDeliverables.mutate({
+        jobId: _id,
+        deliverables,
+      });
+      toast.success(`Job ${name} created successfully`);
     },
   });
 }
 
 // Get Jobs
 
-interface Job {
-  _id: string;
-  name: string;
-  progress: number;
-  type: 'freelance' | 'project';
-  status: 'pending' | 'ongoing' | 'completed' | 'waiting' | 'cancelled';
-  payoutStatus: 'pending' | 'ongoing' | 'completed' | 'waiting' | 'cancelled';
-  recipientCompletedJob: boolean;
-  category: string;
-  description: string;
-  isPrivate: boolean;
-  paymentFee: number;
-  inviteAccepted: boolean;
-  deliveryDate: string;
-  creator: {
-    profile: {
-      talent: {
-        skills: string[];
-        availability: string;
-      };
-    };
-    _id: string;
-    firstName: string;
-    lastName: string;
-    type: string;
-    afroScore: number;
-  };
-  skills: {
-    name: string;
-    color: string;
-  }[];
-}
-
 interface GetJobsParams {
   page?: number;
   limit?: number;
+  category: 'open' | 'created' | 'assigned';
+  status?: 'pending' | 'ongoing' | 'completed' | 'cancelled';
 }
 
 interface GetJobsResponse {
-  page: number;
-  total: number;
   limit: number;
+  page: number;
   pages: number;
+  total: number;
   data: Job[];
 }
 
 async function getJobs(params: GetJobsParams): Promise<GetJobsResponse> {
-  const res = await axios.get('/collection', { params });
+  const res = await axios.get('/collection', {
+    params: {
+      type: 'job',
+      creator: params.category === 'created' ? true : undefined,
+      isPrivate: params.category === 'open' ? false : undefined,
+      receiver: params.category === 'assigned' ? true : undefined,
+      status: params.status,
+    },
+  });
   return res.data.data;
 }
 
@@ -156,6 +103,161 @@ export function useGetJobById(params: GetJobByIdParams) {
     queryKey: ['get-job-by-id', params],
     onError: (error: ApiError) => {
       toast.error(error?.response?.data.message || 'An error occurred');
+    },
+  });
+}
+
+// Update Job
+interface UpdateJobParams {
+  id: string;
+  name: string;
+  category: string;
+  paymentFee: number;
+  description: string;
+  tags?: string[];
+  isPrivate: boolean;
+  deliveryDate: string;
+  deliverables: string[];
+  attachments?: string[];
+}
+
+async function postUpdateJob(params: UpdateJobParams): Promise<Job> {
+  const res = await axios.patch(`/collection/${params.id}`, {
+    ...params,
+    type: 'job',
+    id: undefined,
+    deliverables: undefined,
+  });
+  return res.data.data;
+}
+
+export function useUpdateJob() {
+  const updateJobDeliverables = useAssignJobDeliverables();
+
+  return useMutation({
+    mutationFn: postUpdateJob,
+    mutationKey: ['update-job'],
+    onError: (error: ApiError) => {
+      toast.error(error?.response?.data.message || 'An error occurred');
+    },
+    onSuccess: ({ _id, name }, { deliverables = [] }) => {
+      updateJobDeliverables.mutate({
+        jobId: _id,
+        deliverables,
+      });
+      toast.success(`Job ${name} updated successfully`);
+    },
+  });
+}
+
+// Assign Job Deliverables
+
+interface AssignJobDeliverablesParams {
+  jobId: string;
+  deliverables: string[];
+}
+
+async function postAssignJobDeliverables(params: AssignJobDeliverablesParams): Promise<any> {
+  const deliverables = params.deliverables.map((deliverable) => ({
+    name: deliverable,
+    description: deliverable,
+  }));
+
+  const res = await axios.post(`/collection/many`, {
+    parent: params.jobId,
+    type: 'deliverable',
+    collections: deliverables,
+  });
+  return res.data.data;
+}
+
+export function useAssignJobDeliverables() {
+  return useMutation({
+    mutationFn: postAssignJobDeliverables,
+    mutationKey: ['assign-job-deliverables'],
+    onError: (error: ApiError) => {
+      toast.error(error?.response?.data.message || 'Assigning deliverables failed');
+    },
+  });
+}
+
+// Apply to an open job
+// TODO
+
+// Assign job to talent
+// TODO
+
+// Invite talent to a private job
+interface InviteTalentToPrivateJobParams {
+  jobId: string;
+  talentId: string;
+}
+
+async function postInviteTalentToPrivateJob(params: InviteTalentToPrivateJobParams): Promise<ApiResponse> {
+  const res = await axios.post(`/invite`, {
+    collection: params.jobId,
+    receiver: params.talentId,
+  });
+  return res.data.data;
+}
+
+export function useInviteTalentToPrivateJob() {
+  return useMutation({
+    mutationFn: postInviteTalentToPrivateJob,
+    mutationKey: ['invite-talent-to-private-job'],
+    onError: (error: ApiError) => {
+      toast.error(error?.response?.data.message || 'An error occurred');
+    },
+    onSuccess: () => {
+      toast.success('Talent invited successfully');
+    },
+  });
+}
+
+// Accept a private job invite
+
+interface AcceptPrivateJobInviteParams {
+  inviteId: string;
+}
+
+async function postAcceptPrivateJobInvite(params: AcceptPrivateJobInviteParams): Promise<ApiResponse> {
+  const res = await axios.post(`/invite/${params.inviteId}/accept`);
+  return res.data.data;
+}
+
+export function useAcceptPrivateJobInvite() {
+  return useMutation({
+    mutationFn: postAcceptPrivateJobInvite,
+    mutationKey: ['accept-private-job-invite'],
+    onError: (error: ApiError) => {
+      toast.error(error?.response?.data.message || 'An error occurred');
+    },
+    onSuccess: () => {
+      toast.success('Invite accepted successfully');
+    },
+  });
+}
+
+// Decline a private job invite
+
+interface DeclinePrivateJobInviteParams {
+  inviteId: string;
+}
+
+async function postDeclinePrivateJobInvite(params: DeclinePrivateJobInviteParams): Promise<ApiResponse> {
+  const res = await axios.post(`/invite/${params.inviteId}/decline`);
+  return res.data.data;
+}
+
+export function useDeclinePrivateJobInvite() {
+  return useMutation({
+    mutationFn: postDeclinePrivateJobInvite,
+    mutationKey: ['decline-private-job-invite'],
+    onError: (error: ApiError) => {
+      toast.error(error?.response?.data.message || 'An error occurred');
+    },
+    onSuccess: () => {
+      toast.success('Invite declined successfully');
     },
   });
 }
