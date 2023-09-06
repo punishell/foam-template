15c4a958ae8203d0311bf89dd18f71feee537210
yinkakun <yinkakun@gmail.com>
2023-09-06 18:59:43 +0300

    feat(jobs): implement jobs fetching and presentation, add new deliverables-input component

--- components/jobs/open-jobs.tsx
+++ components/jobs/open-jobs.tsx
@@ -1,27 +1,40 @@
 import React from 'react';
+import type { Job } from '@/lib/types';
 import { Tabs } from '@/components/common/tabs';
 import { JobSearchBar } from '@/components/jobs/job-search-bar';
 import { OpenJobCard } from '@/components/jobs/job-cards/open-job';
 
+import { useGetJobs } from '@/lib/api/job';
+import { PageEmpty } from '@/components/common/page-empty';
+import { PageError } from '@/components/common/page-error';
+import { PageLoading } from '@/components/common/page-loading';
+
 interface Props {}
 
 export const OpenJobs: React.FC<Props> = () => {
+  const jobsData = useGetJobs({ category: 'open' });
+
+  if (jobsData.isError) return <PageError />;
+  if (jobsData.isLoading) return <PageLoading />;
+
+  const jobs = jobsData.data.data;
+
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 h-full">
       <JobSearchBar />
-      <div>
+      <div className="h-ful grow">
         <Tabs
           urlKey="open-jobs"
           tabs={[
             {
               label: 'All',
               value: 'all',
-              content: <AllJobs />,
+              content: <AllJobs jobs={jobs} />,
             },
             {
               label: 'Saved',
               value: 'saved',
-              content: <SavedJobs />,
+              content: <SavedJobs jobs={[]} />,
             },
           ]}
         />
@@ -30,60 +43,61 @@ export const OpenJobs: React.FC<Props> = () => {
   );
 };
 
-interface AllJobsProps {}
+interface AllJobsProps {
+  jobs: Job[];
+}
 
-const AllJobs: React.FC<AllJobsProps> = () => {
+const AllJobs: React.FC<AllJobsProps> = ({ jobs }) => {
+  if (!jobs.length) return <PageEmpty label="No open jobs yet." className="rounded-lg border border-line h-full" />;
+
   return (
-    <div className="grid grid-cols-2 gap-4">
-      <OpenJobCard
-        price={100}
-        title='I need a logo for my new business "Pakt"'
-        skills={['Logo Design', 'Graphic Design', 'Illustration']}
-        creator={{
-          avatar: 'https://i.pravatar.cc/300',
-          name: 'John Doe',
-          paktScore: 100,
-        }}
-      />
-      <OpenJobCard
-        price={100}
-        title='I need a logo for my new business "Pakt"'
-        skills={['Logo Design', 'Graphic Design', 'Illustration']}
-        creator={{
-          avatar: 'https://i.pravatar.cc/300',
-          name: 'John Doe',
-          paktScore: 100,
-        }}
-      />
-      <OpenJobCard
-        price={100}
-        title='I need a logo for my new business "Pakt"'
-        skills={['Logo Design', 'Graphic Design', 'Illustration']}
-        creator={{
-          avatar: 'https://i.pravatar.cc/300',
-          name: 'John Doe',
-          paktScore: 100,
-        }}
-      />
+    <div className="grid grid-cols-2 gap-4 overflow-y-auto pb-20">
+      {jobs.map(({ _id, paymentFee, name, tagsData, creator }) => {
+        return (
+          <OpenJobCard
+            id={_id}
+            key={_id}
+            price={paymentFee}
+            title={name}
+            skills={tagsData}
+            creator={{
+              paktScore: creator.score,
+              avatar: creator.profileImage?.url,
+              name: `${creator.firstName} ${creator.lastName}`,
+            }}
+          />
+        );
+      })}
     </div>
   );
 };
 
-interface SavedJobsProps {}
+interface SavedJobsProps {
+  jobs: Job[];
+}
 
-const SavedJobs: React.FC<SavedJobsProps> = () => {
+const SavedJobs: React.FC<SavedJobsProps> = ({ jobs }) => {
+  if (!jobs.length)
+    return <PageEmpty label="Your saved jobs will appear here." className="rounded-lg border border-line h-full" />;
+
   return (
-    <div className="grid grid-cols-2 gap-4">
-      <OpenJobCard
-        price={100}
-        title='I need a logo for my new business "Pakt"'
-        skills={['Logo Design', 'Graphic Design', 'Illustration']}
-        creator={{
-          avatar: 'https://i.pravatar.cc/300',
-          name: 'John Doe',
-          paktScore: 100,
-        }}
-      />
+    <div className="grid grid-cols-2 gap-4 overflow-y-auto pb-20">
+      {jobs.map(({ _id, paymentFee, name, tagsData, creator }) => {
+        return (
+          <OpenJobCard
+            id={_id}
+            key={_id}
+            price={paymentFee}
+            title={name}
+            skills={tagsData}
+            creator={{
+              paktScore: creator.score,
+              avatar: creator.profileImage?.url,
+              name: `${creator.firstName} ${creator.lastName}`,
+            }}
+          />
+        );
+      })}
     </div>
   );
 };
