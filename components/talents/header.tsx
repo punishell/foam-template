665a0fec79346ae49e5a236d7bde2dca6c9a09b4
yinkakun <yinkakun@gmail.com>
2023-09-18 16:44:18 +0300

    feat(talents): replace UserAvatar with AfroProfile

--- components/talents/header.tsx
+++ components/talents/header.tsx
@@ -3,21 +3,38 @@
 import React from 'react';
 import { Button } from 'pakt-ui';
 import { Briefcase } from 'lucide-react';
-import { UserAvatar } from '@/components/common/user-avatar';
+import { AfroProfile } from '@/components/common/afro-profile';
 import { getAvatarColor } from '@/lib/utils';
 import Link from 'next/link';
 
-export const ProfileHeader = ({ _id, name, position, score, skills }: { _id: string, name: string, position: string, score: number, skills: any[] }) => {
+export const ProfileHeader = ({
+  _id,
+  name,
+  position,
+  score,
+  skills,
+}: {
+  _id: string;
+  name: string;
+  position: string;
+  score: number;
+  skills: any[];
+}) => {
   const borderColor = getAvatarColor(score);
   return (
     <div className="w-full flex relative bg-white py-6 rounded-2xl gap-6 border border-line">
-      <div className="absolute left-[5%] top-[10%] shrink-0">
-        <UserAvatar score={score} size="md" />
+      <div className="absolute left-[5%] top-[0%] shrink-0">
+        <AfroProfile score={Math.round(score)} size="lg">
+          <div className="bg-white h-full w-full"></div>
+        </AfroProfile>
       </div>
       <div className="flex flex-col gap-4 grow">
-        <div className={`flex flex-row gap-2 px-6 w-full justify-between border-b pb-4`} style={{ borderColor: borderColor }}>
-          <div className='w-1/5'></div>
-          <div className='flex flex-row justify-between w-4/5'>
+        <div
+          className={`flex flex-row gap-2 px-6 w-full justify-between border-b pb-4`}
+          style={{ borderColor: borderColor }}
+        >
+          <div className="w-1/5"></div>
+          <div className="flex flex-row justify-between w-4/5">
             <div className="flex flex-col gap-1">
               <h1 className="text-3xl font-bold text-title">{name}</h1>
               <div className="flex gap-2 items-center text-body">
@@ -38,11 +55,17 @@ export const ProfileHeader = ({ _id, name, position, score, skills }: { _id: str
             </div>
           </div>
         </div>
-        <div className='flex flex-row px-6 min-h-[32px]'>
-          <div className='w-1/5'></div>
+        <div className="flex flex-row px-6 min-h-[32px]">
+          <div className="w-1/5"></div>
           <div className="flex w-4/5 flex-wrap gap-2">
             {skills.map((skill, i) => (
-              <span key={i} className="bg-white rounded-full px-6 py-1.5 text-sm font-medium text-[#090A0A]" style={{ backgroundColor: skill.backgroundColor }}>{skill.name}</span>
+              <span
+                key={i}
+                className="bg-white rounded-full px-6 py-1.5 text-sm font-medium text-[#090A0A]"
+                style={{ backgroundColor: skill.backgroundColor }}
+              >
+                {skill.name}
+              </span>
             ))}
           </div>
         </div>
--- components/talents/review.tsx
+++ components/talents/review.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Rating from 'react-rating';
 import { Star } from 'lucide-react';
 import { Carousel } from '@/components/common/carousel';
-import { UserAvatar } from '@/components/common/user-avatar';
+import { AfroProfile } from '@/components/common/afro-profile';
 
 interface ReviewProps {
   body: string;
@@ -27,7 +27,7 @@ const Review: React.FC<ReviewProps> = ({ body, title, rating, user }) => {
 
       <div className="flex justify-between items-center">
         <div className="flex gap-3 items-center">
-          <UserAvatar size="xs" />
+          <AfroProfile size="sm" score={0} />
           <div className="flex flex-col">
             <span className="text-sm text-title font-medium">{user.name}</span>
             <span className="text-sm text-body">{user.title}</span>
@@ -50,24 +50,27 @@ export const Reviews = ({ reviews }: { reviews: any[] }) => {
     <div className="flex flex-col bg-primary-gradient p-4 rounded-4 gap-3 rounded-2xl">
       <h3 className="text-white font-medium text-2xl">Reviews</h3>
       <Carousel>
-        {reviews && reviews.length > 0 && reviews.map((_review, i) =>
-          <Review
-            key={i}
-            title="Great work"
-            body="I have over 13 years of experience crafting award-winning mobile and web apps at well-known tech companies like Google, Nest, and GE, and have worked and consulted at a variety of startups and companies in Silicon Valley. I&nbsp;believe in designing with intention in both work and life. &nbsp; I thrive in bringing clarity to ambiguity"
-            rating={4}
-            user={{
-              name: 'Cameron Williamson',
-              title: 'Full-stack developer',
-              afroScore: 100,
-            }}
-          />
-        )}
-        {!reviews || reviews.length === 0 &&
-          <div className='flex w-full text-white min-h-[307px] my-auto items-center'>
-            <p>No Reviews</p>
-          </div>
-        }
+        {reviews &&
+          reviews.length > 0 &&
+          reviews.map((_review, i) => (
+            <Review
+              key={i}
+              title="Great work"
+              body="I have over 13 years of experience crafting award-winning mobile and web apps at well-known tech companies like Google, Nest, and GE, and have worked and consulted at a variety of startups and companies in Silicon Valley. I&nbsp;believe in designing with intention in both work and life. &nbsp; I thrive in bringing clarity to ambiguity"
+              rating={4}
+              user={{
+                name: 'Cameron Williamson',
+                title: 'Full-stack developer',
+                afroScore: 100,
+              }}
+            />
+          ))}
+        {!reviews ||
+          (reviews.length === 0 && (
+            <div className="flex w-full text-white min-h-[307px] my-auto items-center">
+              <p>No Reviews</p>
+            </div>
+          ))}
       </Carousel>
     </div>
   );
