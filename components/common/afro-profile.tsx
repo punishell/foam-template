1c0278ee08302681d01bfcc9676361c98305c220
yinkakun <yinkakun@gmail.com>
2023-09-18 15:52:32 +0300

    feat(afro-profile): implement afro-profile component

--- components/common/afro-profile.tsx
+++ components/common/afro-profile.tsx
@@ -1,65 +1,158 @@
 import React from 'react';
+import { arc } from 'd3-shape';
 
 type Size = 'sm' | 'md' | 'lg' | 'xl';
-
-interface AfroProfileProps {
-  size: Size;
-  score: number;
-  image: string;
-}
 
 const SIZE_TO_PX: Record<Size, number> = {
   sm: 60,
   md: 110,
   lg: 150,
-  xl: 200,
+  xl: 180,
 };
 
-export const AfroProfile: React.FC<AfroProfileProps> = ({ image, score, size }) => {
+interface AfroProfileProps {
+  size: Size;
+  score: number;
+  children?: React.ReactNode;
+}
+
+const progressToColor = (progress: number) => {
+  if (progress <= 24) {
+    return '#fa0832';
+  } else if (progress <= 49) {
+    return '#ffbf04';
+  } else if (progress <= 74) {
+    return '#649ff9';
+  } else {
+    return '#17a753';
+  }
+};
+
+export const AfroProfile: React.FC<AfroProfileProps> = ({ size, score = 0, children }) => {
+  const id = React.useId();
   const sizeInPx = SIZE_TO_PX[size];
-  const STROKE_WIDTH = sizeInPx * 0.15;
+  const thickness = sizeInPx / 11;
+  const radius = (sizeInPx - thickness) / 2;
+  const progressAngle = (score / 100) * 2 * Math.PI;
 
-  // calculate circumference based on sizeInPx
-  const circumference = sizeInPx * 2 * Math.PI;
+  const bgArcGenerator = arc()
+    .startAngle(0)
+    .outerRadius(radius)
+    .endAngle(2 * Math.PI)
+    .innerRadius(radius - thickness);
 
-  // calculate inactive stroke-dashoffset based on score
-  const offset = ((100 - score) / 100) * circumference;
+  const progressArcGenerator = arc()
+    .startAngle(0)
+    .outerRadius(radius)
+    .endAngle(progressAngle)
+    .innerRadius(radius - thickness)
+    .cornerRadius(666);
 
+  // @ts-ignore
+  const bgArcPath = bgArcGenerator();
+  // @ts-ignore
+  const progressArcPath = progressArcGenerator();
+
+  const knobRadius = thickness * 1.2;
+
+  // Add a knob to the progress arc
+  const knobX = (radius - thickness / 2) * Math.cos(progressAngle - Math.PI / 2);
+  const knobY = (radius - thickness / 2) * Math.sin(progressAngle - Math.PI / 2);
   return (
-    <div className="rounded-full">
-      <svg width={sizeInPx} height={sizeInPx} viewBox={`0 0 ${sizeInPx} ${sizeInPx}`}>
-        <defs>
-          <pattern id="paktscore" patternUnits="userSpaceOnUse" height={sizeInPx} width={sizeInPx}>
-            <image x="0" y="0" height={sizeInPx} width={sizeInPx} xlinkHref="/images/paktscore-gradient.png"></image>
-          </pattern>
-        </defs>
+    <div className="relative flex items-center justify-center ">
+      <div
+        style={{
+          inset: knobRadius,
+          borderRadius: 999,
+          position: 'absolute',
+        }}
+      >
+        <div
+          style={{
+            position: 'relative',
+            height: '100%',
+            width: '100%',
+            borderRadius: '999px',
+            overflow: 'hidden',
+            backgroundColor: 'transparent',
+          }}
+        >
+          {children}
+        </div>
+      </div>
+      <svg
+        width={sizeInPx + knobRadius}
+        height={sizeInPx + knobRadius}
+        viewBox={`0 0 ${sizeInPx + knobRadius} ${sizeInPx + knobRadius}`}
+        style={{
+          transform: 'rotate(180deg)',
+        }}
+      >
+        <style type="text/css">
+          {`
+            .progress-gradient {
+              background-image: conic-gradient(
+                from 0deg at 50% 50%,
+                #fc2533,
+                #fc2533,
+                #ffc005,
+                #ffc005,
+                #ffc005,
+                #649ff9,
+                #649ff9,
+                #649ff9,
+                #04a82a,
+                #04a82a
+              );
+            }
+          `}
+        </style>
 
-        {/* track circle  */}
+        <g transform={`translate(${(sizeInPx + knobRadius) / 2}, ${(sizeInPx + knobRadius) / 2})`}>
+          <clipPath id={id}>
+            <path
+              d={progressArcPath}
+              transform={`translate(${(sizeInPx + knobRadius) / 2}, ${(sizeInPx + knobRadius) / 2})`}
+            />
+          </clipPath>
+          <path d={bgArcPath} fill="#e3e5e5" />
+        </g>
+        <foreignObject
+          x="0"
+          y="0"
+          width={sizeInPx + knobRadius}
+          height={sizeInPx + knobRadius}
+          clipPath={`url(#${id})`}
+        >
+          <div
+            className="progress-gradient"
+            style={{
+              width: sizeInPx + knobRadius,
+              height: sizeInPx + knobRadius,
+              borderRadius: 999,
+            }}
+          />
+        </foreignObject>
         <circle
-          stroke="#ccc"
-          cx={sizeInPx / 2}
-          cy={sizeInPx / 2}
-          fill="transparent"
-          strokeWidth={STROKE_WIDTH}
-          r={sizeInPx / 2 - STROKE_WIDTH / 2}
+          cx={knobX}
+          cy={knobY}
+          r={knobRadius}
+          fill={progressToColor(score)}
+          transform={`translate(${(sizeInPx + knobRadius) / 2}, ${(sizeInPx + knobRadius) / 2})`}
         ></circle>
 
-        {/* progress circle */}
-        <circle
-          id="progressCircle"
-          cx={sizeInPx / 2}
-          cy={sizeInPx / 2}
-          strokeWidth={STROKE_WIDTH}
-          fill="transparent"
-          stroke-linecap="round"
-          stroke="url(#paktscore)"
-          strokeDashoffset={offset}
-          strokeDasharray={circumference}
-          r={sizeInPx / 2 - STROKE_WIDTH / 2}
-        ></circle>
-
-        {/* white background circle  */}
-        <circle cx={sizeInPx / 2} cy={sizeInPx / 2} r={sizeInPx / 2 - STROKE_WIDTH / 2} fill="white"></circle>
+        <text
+          x={knobX + (sizeInPx + knobRadius) / 2}
+          y={knobY + (sizeInPx + knobRadius) / 2}
+          dy=".3em"
+          textAnchor="middle"
+          fill="white"
+          fontWeight={700}
+          transform={`rotate(180, ${knobX + (sizeInPx + knobRadius) / 2}, ${knobY + (sizeInPx + knobRadius) / 2})`}
+          fontSize={Math.round(sizeInPx / 10)}
+        >
+          {`${score}`}
+        </text>
       </svg>
     </div>
   );
