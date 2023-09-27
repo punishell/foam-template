6d5a3b283aaf64c6ffa1178dd129e637d152bc25
yinkakun <yinkakun@gmail.com>
2023-09-27 09:32:03 +0300

    refactor(talent-details): improve data fetching and error handling

--- app/(dashboard)/talents/[talent-id]/page.tsx
+++ app/(dashboard)/talents/[talent-id]/page.tsx
@@ -1,60 +1,65 @@
 'use client';
 
-import React, { useEffect, useMemo } from 'react';
-import { Button } from 'pakt-ui';
-import { Briefcase } from 'lucide-react';
-import { UserAvatar } from '@/components/common/user-avatar';
+import React from 'react';
+import { PageError } from '@/components/common/page-error';
+import { PageLoading } from '@/components/common/page-loading';
 import { useGetTalentById } from '@/lib/api';
-import { useRouter, useParams } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import { Achievements } from '@/components/talents/achievement';
 import { Reviews } from '@/components/talents/review';
-import { Spinner } from '@/components/common';
 import { ProfileHeader } from '@/components/talents/header';
 
 export default function TalentDetails() {
   const params = useParams();
-  const router = useRouter();
-  const talentId = String(params["talent-id"]);
-  const { data: talentData, refetch: FetchTalent, isFetched, isFetching } = useGetTalentById(talentId);
-  console.log(talentData);
-  useEffect(() => {
-    if (talentId) {
-      FetchTalent();
-    } else router.back();
-  }, []);
+  const talentId = String(params['talent-id']);
+  const talentData = useGetTalentById(talentId, true);
 
-  const talent = useMemo(() => ({
-    id: talentData?.talent._id || "",
-    name: `${talentData?.talent?.firstName} ${talentData?.talent?.lastName}`,
-    position: talentData?.talent?.profile?.bio?.title || "",
-    bio: talentData?.talent?.profile?.bio?.description || "",
-    score: talentData?.talent?.score || 0,
-    achievements: (talentData?.talent?.achievements || []).map((a) => ({ title: a.type, type: a.type, total: Number(a.total), value: Number(a.value) })),
-    skills: (talentData?.talent?.profile?.talent?.tagsIds || []).map((t) => ({ name: t.name, backgroundColor: t.color })) || [],
-    reviews: talentData?.review?.data || [],
-  }), [talentData]);
+  if (talentData.isLoading) return <PageLoading />;
 
+  if (talentData.isError) return <PageError />;
 
-  if (!isFetched || isFetching) {
-    return <div className="flex h-full w-full my-auto items-center justify-center z-20"><Spinner /></div>
-  }
+  const talent = talentData.data.talent;
 
   return (
     <div className="flex flex-col gap-6 pt-6 overflow-y-auto">
-      <ProfileHeader _id={talent.id} name={talent.name} position={talent.position} score={talent.score} skills={talent?.skills} />
+      <ProfileHeader
+        _id={talent._id}
+        name={`${talent.firstName} ${talent.lastName}`}
+        position={talent.profile.bio?.title ?? ''}
+        score={talent.score}
+        skills={talent?.profile?.talent?.tagsIds?.map((t) => ({ name: t.name, backgroundColor: t.color })) ?? []}
+        profileImage={talent.profileImage?.url}
+      />
 
       <div className="flex gap-6 w-full">
-        <Bio body={talent.bio} />
-        <Achievements achievements={talent.achievements} />
+        <Bio body={talent.profile.bio?.description ?? ''} />
+        <Achievements
+          achievements={talentData.data.talent.achievements?.map(({ total, type, value }) => ({
+            type,
+            title: type,
+            total: Number(total),
+            value: Number(value),
+          }))}
+        />
       </div>
-      <Reviews reviews={talent.reviews} loading={!isFetched && isFetching} />
+      <Reviews
+        reviews={
+          talent.achievements?.map((a) => ({
+            title: a.type,
+            type: a.type,
+            total: Number(a.total),
+            value: Number(a.value),
+          })) ?? []
+        }
+        loading={talentData.isLoading}
+      />
     </div>
   );
 }
 
 const Bio = ({ body }: { body: string }) => {
   return (
-    <div className="flex flex-col grow-0 w-[60%] bg-[#FFEFD7] p-4 rounded-4 gap-3 border border-yellow-dark rounded-2xl">
+    <div className="flex flex-col grow-0 w-full bg-[#FFEFD7] p-4 rounded-4 gap-3 border border-yellow-dark rounded-2xl">
       <h3 className="text-left text-title text-lg font-medium">Bio</h3>
       <div>{body}</div>
     </div>
