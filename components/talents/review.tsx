e00bcee7ce4d6020bb1d53fef31334d50f8bcfd4
yinkakun <yinkakun@gmail.com>
2023-09-27 09:27:38 +0300

    feat(achievement): update AchievementType values and add title to AchievementTypeMap refactor(achievement.tsx): remove redundant AchievementBarProps title prop refactor(talents/header): add DefaultAvatar, isOwnProfile prop, profileImage prop and InviteTalentModal

--- components/talents/review.tsx
+++ components/talents/review.tsx
@@ -5,7 +5,7 @@ import Rating from 'react-rating';
 import { Star } from 'lucide-react';
 import { Carousel } from '@/components/common/carousel';
 import { AfroProfile } from '@/components/common/afro-profile';
-import { Spinner } from '../common';
+import { Spinner } from '@/components/common/';
 
 interface ReviewProps {
   body: string;
@@ -36,7 +36,7 @@ const Review: React.FC<ReviewProps> = ({ body, title, rating, user }) => {
         </div>
         {/* @ts-ignore */}
         <Rating
-          initialRating={2.5}
+          initialRating={rating}
           fullSymbol={<Star fill="#15D28E" color="#15D28E" />}
           emptySymbol={<Star fill="transparent" color="#15D28E" />}
           readonly
@@ -50,29 +50,7 @@ export const Reviews = ({ reviews, loading }: { reviews: any[]; loading: boolean
   return (
     <div className="flex flex-col bg-primary-gradient p-4 rounded-4 gap-3 rounded-2xl">
       <h3 className="text-white font-medium text-2xl">Reviews</h3>
-      <Carousel>
-        {reviews &&
-          reviews.length > 0 &&
-          reviews.map((_review, i) => (
-            <Review
-              key={i}
-              title="Great work"
-              body="I have over 13 years of experience crafting award-winning mobile and web apps at well-known tech companies like Google, Nest, and GE, and have worked and consulted at a variety of startups and companies in Silicon Valley. I&nbsp;believe in designing with intention in both work and life. &nbsp; I thrive in bringing clarity to ambiguity"
-              rating={4}
-              user={{
-                name: 'Cameron Williamson',
-                title: 'Full-stack developer',
-                afroScore: 100,
-              }}
-            />
-          ))}
-        {!reviews ||
-          (reviews.length === 0 && (
-            <div className="flex w-full text-white min-h-[307px] my-auto items-center">
-              <p>No Reviews</p>
-            </div>
-          ))}
-      </Carousel>
+
       {loading ? (
         <div className="flex  min-h-[307px] w-full text-white my-auto items-center justify-center z-20">
           <Spinner />
