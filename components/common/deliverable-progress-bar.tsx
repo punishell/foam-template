820f0966a9e80004d3854a13b44789c756bb1c81
yinkakun <yinkakun@gmail.com>
2023-10-05 07:59:28 +0300

    refactor(DeliverableProgressBar): modify element styles and className prop

--- components/common/deliverable-progress-bar.tsx
+++ components/common/deliverable-progress-bar.tsx
@@ -1,13 +1,16 @@
+import { cn } from '@/lib/utils';
+
 interface Props {
+  className?: string;
   totalDeliverables: number;
   percentageProgress: number;
 }
 
-const InProgress = () => <div className="bg-line rounded-full h-2 w-[30px] lg:w-[60px]" />;
+const InProgress = () => <div className="bg-line rounded-full h-2 flex-1 basis-0" />;
 
-const Completed = () => <div className="bg-[#23C16B] rounded-full h-2 w-[30px] lg:w-[60px]" />;
+const Completed = () => <div className="bg-[#23C16B] rounded-full h-2 flex-1 basis-0" />;
 
-export const DeliverableProgressBar: React.FC<Props> = ({ percentageProgress, totalDeliverables }) => {
+export const DeliverableProgressBar: React.FC<Props> = ({ percentageProgress, totalDeliverables, className }) => {
   const progressBars = [];
 
   for (let i = 0; i < totalDeliverables; i++) {
@@ -19,12 +22,12 @@ export const DeliverableProgressBar: React.FC<Props> = ({ percentageProgress, to
   }
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex gap-2 items-center text-[#6E7191] text-xs">
-        <span>Progress</span>
+    <div className={cn('flex flex-col gap-2 w-full max-w-[200px] text-xs', className)}>
+      <div className="flex gap-2 items-center text-[#6E7191]">
+        <span>Job Progress:</span>
         <span>{percentageProgress}%</span>
       </div>
-      <div className="flex items-center gap-1">{progressBars}</div>
+      <div className="flex items-center gap-1 grow">{progressBars}</div>
     </div>
   );
 };
