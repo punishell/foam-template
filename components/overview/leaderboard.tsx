1ab91b4a5f10e41adf6ee70a0bdffb55eabeff3e
yinkakun <yinkakun@gmail.com>
2023-10-09 11:56:29 +0300

    feat(leaderboard): truncate leaderboard usernames

--- components/overview/leaderboard.tsx
+++ components/overview/leaderboard.tsx
@@ -1,8 +1,9 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import { useGetLeaderBoard } from '@/lib/api/dashboard';
 import { Spinner } from '../common';
 import { ProfileImage } from './ProfileImage';
+import { truncate } from '@/lib/utils';
 
 export const LeaderBoard = () => {
   const { data: leaderboardData, isFetched, isFetching } = useGetLeaderBoard();
@@ -13,18 +14,17 @@ export const LeaderBoard = () => {
     position: i + 1,
   }));
   return (
-    <div className="flex flex-col grow w-full">
-      <div className="text-xl font-bold text-center mb-6">Leaderboard</div>
-      <div className="relative w-full bg-gradient-leaderboard rounded-2xl grow overflow-y-auto">
-        <div className=" text-white px-3 py-6 flex flex-col gap-4">
-          {!isFetched && isFetching && <Spinner />}
-          {leaderboard.map((l, i) => {
-            if (l.position == 1) return <FirstPlace key={i} name={l.name} score={l.score} avatar={l.image} />;
-            if (l.position == 2) return <SecondPlace key={i} name={l.name} score={l.score} avatar={l.image} />;
-            if (l.position == 3) return <ThirdPlace key={i} name={l.name} score={l.score} avatar={l.image} />;
-            return <RunnerUp key={i} name={l.name} score={l.score} place={`${l.position}th`} avatar={l.image} />;
-          })}
-        </div>
+    <div className="flex flex-col h-fit w-full gap-2 bg-gradient-leaderboard rounded-2xl py-4">
+      <div className="text-xl font-bold text-center text-white">Leaderboard</div>
+
+      <div className=" text-white px-3 flex flex-col  relative gap-2 overflow-y-auto scrollbar-hide">
+        {!isFetched && isFetching && <Spinner />}
+        {leaderboard.map((l, i) => {
+          if (l.position == 1) return <FirstPlace key={i} name={l.name} score={l.score} avatar={l.image} />;
+          if (l.position == 2) return <SecondPlace key={i} name={l.name} score={l.score} avatar={l.image} />;
+          if (l.position == 3) return <ThirdPlace key={i} name={l.name} score={l.score} avatar={l.image} />;
+          return <RunnerUp key={i} name={l.name} score={l.score} place={`${l.position}th`} avatar={l.image} />;
+        })}
       </div>
     </div>
   );
@@ -79,10 +79,10 @@ const FirstPlace: React.FC<LeaderBoardItemProps> = ({ name, score, avatar }) => 
           </linearGradient>
         </defs>
       </svg>
-      <div className="absolute inset-0 gap-3 flex items-center p-3 pl-1">
+      <div className="absolute inset-0 gap-2 flex items-center p-3 pl-1">
         <ProfileImage imageUrl={avatar} score={Math.round(score)} size="sm" />
         <div className="grow">
-          <span className="text-[#ECFCE5] text-lg">{name}</span>
+          <span className="text-[#ECFCE5] text-base">{truncate(name, 15)}</span>
           <div className="flex gap-2 justify-between items-center">
             <span className="text-sm text-[#F2F4F5]">Afroscore: {Math.round(score)}</span>
             <Image src="/icons/medal-1.png" width={28} height={28} alt="" />
@@ -140,10 +140,10 @@ const SecondPlace: React.FC<LeaderBoardItemProps> = ({ name, score, avatar }) =>
           </linearGradient>
         </defs>
       </svg>
-      <div className="absolute inset-0 gap-3 flex items-center p-3 pl-1">
+      <div className="absolute inset-0 gap-2 flex items-center p-3 pl-1">
         <ProfileImage imageUrl={avatar} score={Math.round(score)} size="sm" />
         <div className="grow">
-          <span className="text-[#ECFCE5] text-lg">{name}</span>
+          <span className="text-[#ECFCE5] text-base">{truncate(name, 15)}</span>
           <div className="flex gap-2 justify-between items-center">
             <span className="text-sm text-[#F2F4F5]">Afroscore: {Math.round(score)}</span>
             <Image src="/icons/medal-2.png" width={28} height={28} alt="" />
@@ -196,10 +196,10 @@ const ThirdPlace: React.FC<LeaderBoardItemProps> = ({ name, score, avatar }) => 
           </linearGradient>
         </defs>
       </svg>
-      <div className="absolute inset-0 gap-3 flex items-center p-3 pl-1">
+      <div className="absolute inset-0 gap-2 flex items-center p-3 pl-1">
         <ProfileImage imageUrl={avatar} score={Math.round(score)} size="sm" />
         <div className="grow">
-          <span className="text-[#ECFCE5] text-lg">{name}</span>
+          <span className="text-[#ECFCE5] text-base">{truncate(name, 15)}</span>
           <div className="flex gap-2 justify-between items-center">
             <span className="text-sm text-[#F2F4F5]">Afroscore: {Math.round(score)}</span>
             <Image src="/icons/medal-3.png" width={28} height={28} alt="" />
@@ -219,10 +219,10 @@ const RunnerUp: React.FC<LeaderBoardItemProps> = ({ name, score, avatar, place }
           d="M0 16.468C0 7.585 7.198.402 16.08.463 43.22.65 97.673 1 123.5 1c25.828 0 80.281-.35 107.419-.537C239.802.403 247 7.585 247 16.468V45.42c0 8.797-7.076 15.936-15.873 15.962-17.277.052-50.402.118-107.627.118-57.225 0-90.35-.066-107.627-.118C7.076 61.356 0 54.217 0 45.42V16.468z"
         ></path>
       </svg>
-      <div className="absolute inset-0 gap-3 flex items-center p-3 pl-1">
+      <div className="absolute inset-0 gap-2 flex items-center p-3 pl-1">
         <ProfileImage imageUrl={avatar} score={Math.round(score)} size="sm" />
         <div className="grow">
-          <span className="text-[#ECFCE5] text-lg">{name}</span>
+          <span className="text-[#ECFCE5] text-base ">{truncate(name, 15)}</span>
           <div className="flex gap-2 justify-between items-center">
             <span className="text-sm text-[#F2F4F5]">Afroscore: {Math.round(score)}</span>
             <span className="text-sm text-[#CDCFD0]">{place}</span>
