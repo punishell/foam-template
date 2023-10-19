7274ada542d2557e7e9fac84aadab5b7f2f29cb3
yinkakun <yinkakun@gmail.com>
2023-10-19 17:01:59 +0300

    feat(2fa): remove security question

--- components/settings/security/auth-app.tsx
+++ components/settings/security/auth-app.tsx
@@ -38,7 +38,7 @@ export const AuthApp2FA = ({ isEnabled, disabled }: AuthApp2FA) => {
         <div className="absolute right-4 top-4">
           <Checkbox checked={isEnabled} />
         </div>
-        <div className="flex h-[75px] items-center">
+        <div className="flex h-[100px] items-center">
           <Image src="/icons/authenticator-app.svg" width={76} height={76} alt="" />
         </div>
         <Text.p size="lg">Authenticator app</Text.p>
--- components/settings/security/index.tsx
+++ components/settings/security/index.tsx
@@ -1,30 +1,34 @@
-import { Button, Input, Text } from "pakt-ui";
+import { Button, Input, Text } from 'pakt-ui';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { Check } from "lucide-react";
-import { AuthApp2FA } from "./auth-app";
-import { Email2FA } from "./email-2fa";
-import { SecurityQuestion2FA } from "./security-question-2fa";
-import { useChangePassword } from "@/lib/api/account";
-import { Spinner } from "@/components/common";
-import { useMemo } from "react";
-import { useUserState } from "@/lib/store/account";
-import { TWO_FA_CONSTANTS } from "@/lib/constants";
-import { spChars } from "@/lib/utils";
+import { Check } from 'lucide-react';
+import { AuthApp2FA } from './auth-app';
+import { Email2FA } from './email-2fa';
+import { SecurityQuestion2FA } from './security-question-2fa';
+import { useChangePassword } from '@/lib/api/account';
+import { Spinner } from '@/components/common';
+import { useMemo } from 'react';
+import { useUserState } from '@/lib/store/account';
+import { TWO_FA_CONSTANTS } from '@/lib/constants';
+import { spChars } from '@/lib/utils';
 
-
-const changePasswordFormSchema = z.object({
-  currentPassword: z.string().min(1, 'Current Password is required'),
-  newPassword: z.string().min(1, 'New Password is required').regex(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-    "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
-  ),
-  confirmNewPassword: z.string().min(1, 'Confirm New Password is required'),
-}).refine((data) => data.newPassword === data.confirmNewPassword, {
-  message: "Passwords don't match",
-  path: ["confirmNewPassword"],
-});
+const changePasswordFormSchema = z
+  .object({
+    currentPassword: z.string().min(1, 'Current Password is required'),
+    newPassword: z
+      .string()
+      .min(1, 'New Password is required')
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+        'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character',
+      ),
+    confirmNewPassword: z.string().min(1, 'Confirm New Password is required'),
+  })
+  .refine((data) => data.newPassword === data.confirmNewPassword, {
+    message: "Passwords don't match",
+    path: ['confirmNewPassword'],
+  });
 
 type EditProfileFormValues = z.infer<typeof changePasswordFormSchema>;
 
@@ -37,44 +41,68 @@ export const SecurityView = () => {
 
   const form = useForm<EditProfileFormValues>({
     resolver: zodResolver(changePasswordFormSchema),
-    reValidateMode: "onChange",
+    reValidateMode: 'onChange',
   });
 
   const onSubmit: SubmitHandler<EditProfileFormValues> = (values) => {
-    return changePassword.mutate({ oldPassword: values.currentPassword, newPassword: values.newPassword }, {
-      onSuccess: () => {
-        form.reset({
-          currentPassword: "",
-          newPassword: "",
-          confirmNewPassword: ""
-        });
-      }
-    });
+    return changePassword.mutate(
+      { oldPassword: values.currentPassword, newPassword: values.newPassword },
+      {
+        onSuccess: () => {
+          form.reset({
+            currentPassword: '',
+            newPassword: '',
+            confirmNewPassword: '',
+          });
+        },
+      },
+    );
   };
-  const validatingErr = useMemo(() => ({
-    isMinLength: form.getValues().newPassword && form.getValues().newPassword.length >= 8 || false,
-    checkLowerUpper: form.getValues().newPassword && /[A-Z]/.test(form.getValues().newPassword) && /[a-z]/.test(form.getValues().newPassword) || false,
-    checkNumber: form.getValues().newPassword && form.getValues().newPassword.match(/\d+/g) ? true : false,
-    specialCharacter: form.getValues().newPassword && spChars.test(form.getValues().newPassword) || false,
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }), [form.watch("newPassword")]);
+  const validatingErr = useMemo(
+    () => ({
+      isMinLength: (form.getValues().newPassword && form.getValues().newPassword.length >= 8) || false,
+      checkLowerUpper:
+        (form.getValues().newPassword &&
+          /[A-Z]/.test(form.getValues().newPassword) &&
+          /[a-z]/.test(form.getValues().newPassword)) ||
+        false,
+      checkNumber: form.getValues().newPassword && form.getValues().newPassword.match(/\d+/g) ? true : false,
+      specialCharacter: (form.getValues().newPassword && spChars.test(form.getValues().newPassword)) || false,
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }),
+    [form.watch('newPassword')],
+  );
 
   return (
-    <div className='flex flex-row relative gap-6 h-full grow overflow-auto pb-4'>
-      <div className='flex flex-col w-1/4 h-fit bg-white rounded-lg'>
-        <form className='flex flex-col gap-4 p-4'
+    <div className="flex flex-row relative gap-6 h-full grow overflow-auto pb-4">
+      <div className="flex flex-col w-1/4 h-fit bg-white rounded-lg">
+        <form
+          className="flex flex-col gap-4 p-4"
           onSubmit={form.handleSubmit(onSubmit)}
           onKeyDown={(e) => {
             if (e.key === 'Enter') {
               e.preventDefault();
             }
-          }}>
+          }}
+        >
           <h3 className="font-bold text-lg">Change Password</h3>
-          <Input {...form.register('currentPassword')} type="password" className="w-full !bg-[#FCFCFD] !border-[#E8E8E8]" placeholder="enter current password" label='Current Password' />
+          <Input
+            {...form.register('currentPassword')}
+            type="password"
+            className="w-full !bg-[#FCFCFD] !border-[#E8E8E8]"
+            placeholder="enter current password"
+            label="Current Password"
+          />
           {form.formState.errors.currentPassword?.message && (
             <span className="text-sm text-red-500">{form.formState.errors.currentPassword?.message}</span>
           )}
-          <Input {...form.register('newPassword')} type="password" className="w-full !bg-[#FCFCFD] !border-[#E8E8E8]" placeholder="enter new password" label='New Password' />
+          <Input
+            {...form.register('newPassword')}
+            type="password"
+            className="w-full !bg-[#FCFCFD] !border-[#E8E8E8]"
+            placeholder="enter new password"
+            label="New Password"
+          />
           <p className="text-body text-sm">Password must contain</p>
           <div className="flex flex-col p-4 text-body text-xs gap-4">
             <PasswordCriteria isValidated={validatingErr.isMinLength} criteria="At least 8 characters" />
@@ -82,11 +110,26 @@ export const SecurityView = () => {
             <PasswordCriteria isValidated={validatingErr.checkNumber} criteria="1 or mote numbers" />
             <PasswordCriteria isValidated={validatingErr.specialCharacter} criteria="1 or more special characters" />
           </div>
-          <Input {...form.register('confirmNewPassword')} type="password" className="w-full !bg-[#FCFCFD] !border-[#E8E8E8]" placeholder="re-enter new password" label='Confirm New Password' />
+          <Input
+            {...form.register('confirmNewPassword')}
+            type="password"
+            className="w-full !bg-[#FCFCFD] !border-[#E8E8E8]"
+            placeholder="re-enter new password"
+            label="Confirm New Password"
+          />
           {form.formState.errors.confirmNewPassword?.message && (
             <span className="text-sm text-red-500">{form.formState.errors.confirmNewPassword?.message}</span>
           )}
-          <Button title='Save Changes' variant={"secondary"} size="sm" type="submit" className="min-w-[136px]" disabled={!form.formState.isValid || changePassword.isLoading}>{changePassword.isLoading ? <Spinner /> : "Save Changes"}</Button>
+          <Button
+            title="Save Changes"
+            variant={'secondary'}
+            size="sm"
+            type="submit"
+            className="min-w-[136px]"
+            disabled={!form.formState.isValid || changePassword.isLoading}
+          >
+            {changePassword.isLoading ? <Spinner /> : 'Save Changes'}
+          </Button>
         </form>
       </div>
       <div className="flex h-fit w-3/4 flex-col gap-6 rounded-lg bg-white p-6 ">
@@ -95,20 +138,24 @@ export const SecurityView = () => {
         <div className="flex justify-between gap-5">
           <AuthApp2FA isEnabled={is2FASetUp} disabled={isSecuritySetUp || isEmailSetUp} />
           <Email2FA isEnabled={isEmailSetUp} disabled={is2FASetUp || isSecuritySetUp} />
-          <SecurityQuestion2FA isEnabled={isSecuritySetUp} disabled={is2FASetUp || isEmailSetUp} />
+          {/* <SecurityQuestion2FA isEnabled={isSecuritySetUp} disabled={is2FASetUp || isEmailSetUp} /> */}
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export const PasswordCriteria: React.FC<{
   isValidated: boolean;
   criteria: string;
-  isSignUp?: boolean
+  isSignUp?: boolean;
 }> = ({ isValidated, criteria, isSignUp }) => {
   return (
-    <div className={`flex flex-row gap-4 items-center ${isValidated ? "text-success" : `${isSignUp ? "text-white" : "text-body"}`}`}>
+    <div
+      className={`flex flex-row gap-4 items-center ${
+        isValidated ? 'text-success' : `${isSignUp ? 'text-white' : 'text-body'}`
+      }`}
+    >
       <Check size={15} />
       {criteria}
     </div>
