ce24cc18d4bcfef9689005cf037721be7a66e4fa
yinkakun <yinkakun@gmail.com>
2023-10-20 18:01:28 +0300

    feat(talent): make search bar respond on change

--- app/(dashboard)/talents/page.tsx
+++ app/(dashboard)/talents/page.tsx
@@ -1,19 +1,50 @@
 'use client';
-/* eslint-disable no-unused-vars */
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useMemo, useState } from 'react';
-import { useSearchParams, useRouter, usePathname } from 'next/navigation';
-import { JobSearchBar } from '@/components/jobs/job-search-bar';
+
+import { useDebounce } from 'usehooks-ts';
 import { useGetTalents } from '@/lib/api';
+import { createQueryStrings2 } from '@/lib/utils';
+import React, { useEffect, useMemo, useState } from 'react';
 import { TalentList } from '@/components/talents/talentList';
-import { parseFilterObjectToString } from '@/lib/utils';
+import { NumericInput } from '@/components/common/numeric-input';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
 
 export default function Talents() {
   const [isSearching, setIsSearching] = useState(true);
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const queryParams = new URLSearchParams(searchParams as any);
+
   const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const [searchQuery, setSearchQuery] = useState(searchParams.get('search') || '');
+  const debouncedSearchQuery = useDebounce(searchQuery, 300);
+
+  const [skillsQuery, setSkillsQuery] = useState(searchParams.get('skills') || '');
+  const debouncedSkillsQuery = useDebounce(skillsQuery, 300);
+
+  const [minimumPriceQuery, setMinimumPriceQuery] = useState(searchParams.get('range')?.split(',')[0] || 0);
+  const debouncedMinimumPriceQuery = useDebounce(minimumPriceQuery, 300);
+
+  const [maximumPriceQuery, setMaximumPriceQuery] = useState(searchParams.get('range')?.split(',')[1] || 100);
+  const debouncedMaximumPriceQuery = useDebounce(maximumPriceQuery, 300);
+
+  React.useEffect(() => {
+    const queries = createQueryStrings2({
+      skills: debouncedSkillsQuery ?? '',
+      search: debouncedSearchQuery ?? '',
+      range: `${debouncedMinimumPriceQuery ?? 0},${debouncedMaximumPriceQuery ?? 100}`,
+    });
+
+    router.push(`${pathname}?${queries}`);
+  }, [
+    router,
+    pathname,
+    debouncedSearchQuery,
+    debouncedSkillsQuery,
+    debouncedMinimumPriceQuery,
+    debouncedMaximumPriceQuery,
+  ]);
+
+  const queryParams = new URLSearchParams(searchParams as any);
   const Limit = queryParams.get('limit') || '6';
   const page = queryParams.get('page') || '1';
   const searchQ = queryParams.get('search') || '';
@@ -66,22 +97,63 @@ export default function Talents() {
 
   useEffect(() => {
     fetchTalents();
-  }, [searchParams]);
+  }, [searchParams, fetchTalents]);
 
   const handlePagination = (page: number) => {
     setIsSearching(true);
     return router.push(`${pathname}?${createQueryString('page', String(page))}`);
   };
 
-  const handleSearch = (data: Record<string, any>) => {
-    const query = parseFilterObjectToString(data);
-    setIsSearching(true);
-    return router.push(`${pathname}?${query}`);
-  };
-
   return (
     <div className="flex h-full flex-col gap-6 overflow-auto">
-      <JobSearchBar handleSearch={handleSearch} search={searchQ} skills={skillQ} range={rangeQ} isTalentView />
+      <div className="bg-white border-[#7DDE86] border p-6 w-full rounded-2xl flex gap-4 items-end">
+        <div className="flex flex-col relative grow gap-1">
+          <label htmlFor="" className="text-sm">
+            Search
+          </label>
+          <input
+            type="text"
+            value={searchQuery}
+            placeholder="Name, Category, etc."
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="bg-gray-50 px-3 border border-line rounded-lg h-11 focus:outline-none"
+          />
+        </div>
+        <div className="flex flex-col relative grow gap-1">
+          <label htmlFor="" className="text-sm">
+            Skill
+          </label>
+          <input
+            type="text"
+            value={skillsQuery}
+            placeholder="Java, Solidity, etc."
+            onChange={(e) => setSkillsQuery(e.target.value)}
+            className="bg-gray-50 px-3 border border-line rounded-lg h-11 focus:outline-none"
+          />
+        </div>
+        <div className="flex flex-col relative grow gap-1">
+          <label htmlFor="" className="text-sm">
+            Afroscore
+          </label>
+          <div className="flex gap-2 border py-2 border-line rounded-lg h-11 bg-gray-50">
+            <NumericInput
+              type="text"
+              value={minimumPriceQuery}
+              placeholder="From"
+              onChange={(e) => setMinimumPriceQuery(e.target.value)}
+              className="grow focus:outline-none bg-transparent px-3 placeholder:text-sm"
+            />
+            <div className="border-r border-line" />
+            <NumericInput
+              type="text"
+              placeholder="To"
+              value={maximumPriceQuery}
+              onChange={(e) => setMaximumPriceQuery(e.target.value)}
+              className="grow focus:outline-none bg-transparent px-3 placeholder:text-sm"
+            />
+          </div>
+        </div>
+      </div>
 
       <TalentList
         isLoading={(!isFetched && isFetching) || isSearching}
