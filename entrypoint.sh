97434519a441a9822fb65602ddd0a5e052c7c143
Joshua Akinsuyi <akinsuyi.joshua84@gmail.com>
2023-10-25 10:25:09 +0300

    fix: updated env for deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ function apply_path {
     # if config found
     if [ -n "$configValue" ] && [ -n "$envValue" ]; then
       # replace all
-      echo "Replace: ${configValue} with: ${envValue}"
+      echo "Replace: ${configValue} with: ${envValue} for: ${configName}"
       find $nextFolder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#$configValue#$envValue#g"
     fi
   done < $envFilename
