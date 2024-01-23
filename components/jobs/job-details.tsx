d510b4e03046942cbd893ec97bd4e7bd386e35fd
Jayprecode <jayprecode@gmail.com>
2024-01-23 01:26:54 +0100

    🔨 (fix): handled due date error

--- components/jobs/job-details.tsx
+++ components/jobs/job-details.tsx
@@ -28,7 +28,9 @@ interface JobHeaderProps {
 }
 
 export const JobHeader = ({ title, price, dueDate, creator }: JobHeaderProps): ReactElement => {
-    // console.log(dueDate);
+    // Handle due date error
+    const dueDateError = new Date(dueDate).toString() === "Invalid Date";
+    const dueDateValue = dueDateError ? new Date() : new Date(dueDate);
     return (
         <div className="flex items-center justify-between gap-4 rounded-t-xl bg-primary-gradient p-4">
             <div className="flex h-full w-full max-w-2xl flex-col gap-6">
@@ -43,7 +45,7 @@ export const JobHeader = ({ title, price, dueDate, creator }: JobHeaderProps): R
 
                     <span className="flex items-center gap-2 rounded-full bg-[#C9F0FF] px-3 py-1 text-[#0065D0]">
                         <Calendar size={20} />
-                        <span>Due {format(new Date(dueDate), "MMM dd, yyyy")}</span>
+                        <span>Due {format(dueDateValue, "MMM dd, yyyy")}</span>
                     </span>
                 </div>
             </div>
